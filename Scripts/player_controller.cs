e0132dfab0fadfc5886fc3c1a9d54bac4ae1207f
Pnoenix <Ph03n1x.V7@gmail.com>
2026-03-06 13:54:18 +0100

    changed player

--- Scripts/player_controller.cs
+++ Scripts/player_controller.cs
@@ -5,9 +5,9 @@ using System.Threading;
 
 public partial class player_controller : CharacterBody2D
 {
-	[Export] public float Speed = 220.0f;
+	[Export] public float MaxSpeed = 220.0f;
 	[Export] public float Acceleration = 400.0f;
-    [Export] public float Deceleration = 600.0f;
+    [Export] public float Deceleration = 220.0f;
 	[Export] public float RopeSpeed = 150.0f;
 	[Export] private AnimatedSprite2D animationPlayer;
     private bool isMounted = false;
@@ -15,110 +15,152 @@ public partial class player_controller : CharacterBody2D
 
     public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
+        Vector2 velocity = Velocity;
 
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}// Gravity
-
-		if (Input.IsActionJustPressed("ui_accept"))
-		{
-			if (IsOnFloor())
-			{
-                isMounted = true;
-				GD.Print("Player mounted!");
-            }
-			else
-			{
-				isMounted = false;
-                GD.Print("Player unmounted");
-            }
-		}// Mounting and unmounting the rope
-
-		Vector2 inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-
-        if (inputDirection == Vector2.Left)
-		{
-			if (IsOnFloor() && !isMounted)
-			{
-                if(Mathf.Abs(velocity.X) < Speed)
-                    velocity.X -= Acceleration * (float)delta;
-                if(velocity.X != 0)
-                    IsWantedFlip = true;
-            }
-            else
-                isMounted = false;
-		}
-		else if (inputDirection == Vector2.Right)
-		{
-            if (IsOnFloor() && !isMounted)
-            {
-                if (velocity.X < Speed)
-                    velocity.X += Acceleration * (float)delta;
-                if (velocity.X != 0)
-                    IsWantedFlip = false;
-            }
-            else
-                isMounted = false;
-        }
-		else if (inputDirection == Vector2.Up && isMounted)
-		{
-			animationPlayer.FlipH = false;
-			velocity.Y = -RopeSpeed;
-			animationPlayer.Play("rope");
-		}
-		else if (inputDirection == Vector2.Down && isMounted)
-		{
-			animationPlayer.FlipH = false;
-			velocity.Y = RopeSpeed;
-			animationPlayer.Play("rope");
-		}
-
-        //Mathf.MoveToward();
-
-        if (Math.Abs(velocity.X) < Speed) // Animation
-        {
-            if (Math.Abs(velocity.X) != 0 && Math.Abs(velocity.X) < Speed / 5)
-            {
-                ShowSpecificFrame(animationPlayer, "acceleration", 0);
-                animationPlayer.FlipH = IsWantedFlip;
-            }
-            else if (Math.Abs(velocity.X) >= Speed / 5 && Math.Abs(velocity.X) < Speed / 4)
-            {
-                ShowSpecificFrame(animationPlayer, "acceleration", 1);
-            }
-            else if (Math.Abs(velocity.X) >= Speed / 4 && Math.Abs(velocity.X) < Speed / 3)
-            {
-                ShowSpecificFrame(animationPlayer, "acceleration", 2);
-            }
-            else if (Math.Abs(velocity.X) >= Speed / 3 && Math.Abs(velocity.X) < Speed / 2)
-            {
-                ShowSpecificFrame(animationPlayer, "acceleration", 2);
-            }
-            else if (Math.Abs(velocity.X) > Speed / 2 && Math.Abs(velocity.X) < Speed)
-                ShowSpecificFrame(animationPlayer, "acceleration", 3);
-        }
-        else if (Math.Abs(velocity.X) >= Speed)
-        {
-            animationPlayer.Play("move");
-        } 
+        if (!IsOnFloor() && !isMounted)
+            velocity += GetGravity() * (float)delta;
         
-        if (velocity.X == 0)
+        Vector2 inputDirection = Input.GetVector(
+            "ui_left", "ui_right", "ui_up", "ui_down"
+        );
+
+        if (inputDirection.X == 0.0f)
         {
-            animationPlayer.Play("idle");
+            velocity.X = Mathf.MoveToward(
+                velocity.X, 0,
+                Deceleration * (float)delta
+            );
+        } 
+        else
+        {
+            velocity.X = Mathf.MoveToward(
+                velocity.X, 
+                MaxSpeed * inputDirection.X,
+                Acceleration * (float)delta
+            );
+        } 
+
+        if (Mathf.Abs(velocity.X) != MaxSpeed && !isMounted)
+        {
+            ShowSpecificFrame(
+                animationPlayer, 
+                "acceleration", 
+                (int)(Mathf.Abs(Velocity.X) / (MaxSpeed / 5.0))
+            );
+        }
+
+        if (Velocity.X > 0.0)
             animationPlayer.FlipH = false;
-        }
-        if (inputDirection == Vector2.Zero && velocity.X != 0)
-        {
-            if(velocity.X < 0)
-                velocity.X += Deceleration * (float)delta;
-            if (velocity.X > 0)
-                velocity.X -= Deceleration * (float)delta;
-        }
+        if (Velocity.X < 0.0)
+            animationPlayer.FlipH = true;
 
         Velocity = velocity;
-		MoveAndSlide();
+        MoveAndSlide();
+
+		// Vector2 velocity = Velocity;
+
+		// if (!IsOnFloor())
+		// {
+		// 	velocity += GetGravity() * (float)delta;
+		// }// Gravity
+
+		// if (Input.IsActionJustPressed("ui_accept"))
+		// {
+		// 	if (IsOnFloor())
+		// 	{
+        //         isMounted = true;
+		// 		GD.Print("Player mounted!");
+        //     }
+		// 	else
+		// 	{
+		// 		isMounted = false;
+        //         GD.Print("Player unmounted");
+        //     }
+		// }// Mounting and unmounting the rope
+
+		// Vector2 inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+
+        // if (inputDirection == Vector2.Left)
+		// {
+		// 	if (IsOnFloor() && !isMounted)
+		// 	{
+        //         if(Mathf.Abs(velocity.X) < Speed)
+        //             velocity.X -= Acceleration * (float)delta;
+        //         if(velocity.X != 0)
+        //             IsWantedFlip = true;
+        //     }
+        //     else
+        //         isMounted = false;
+		// }
+		// else if (inputDirection == Vector2.Right)
+		// {
+        //     if (IsOnFloor() && !isMounted)
+        //     {
+        //         if (velocity.X < Speed)
+        //             velocity.X += Acceleration * (float)delta;
+        //         if (velocity.X != 0)
+        //             IsWantedFlip = false;
+        //     }
+        //     else
+        //         isMounted = false;
+        // }
+		// else if (inputDirection == Vector2.Up && isMounted)
+		// {
+		// 	animationPlayer.FlipH = false;
+		// 	velocity.Y = -RopeSpeed;
+		// 	animationPlayer.Play("rope");
+		// }
+		// else if (inputDirection == Vector2.Down && isMounted)
+		// {
+		// 	animationPlayer.FlipH = false;
+		// 	velocity.Y = RopeSpeed;
+		// 	animationPlayer.Play("rope");
+		// }
+
+        // //Mathf.MoveToward();
+
+        // if (Math.Abs(velocity.X) < Speed) // Animation
+        // {
+        //     if (Math.Abs(velocity.X) != 0 && Math.Abs(velocity.X) < Speed / 5)
+        //     {
+        //         ShowSpecificFrame(animationPlayer, "acceleration", 0);
+        //         animationPlayer.FlipH = IsWantedFlip;
+        //     }
+        //     else if (Math.Abs(velocity.X) >= Speed / 5 && Math.Abs(velocity.X) < Speed / 4)
+        //     {
+        //         ShowSpecificFrame(animationPlayer, "acceleration", 1);
+        //     }
+        //     else if (Math.Abs(velocity.X) >= Speed / 4 && Math.Abs(velocity.X) < Speed / 3)
+        //     {
+        //         ShowSpecificFrame(animationPlayer, "acceleration", 2);
+        //     }
+        //     else if (Math.Abs(velocity.X) >= Speed / 3 && Math.Abs(velocity.X) < Speed / 2)
+        //     {
+        //         ShowSpecificFrame(animationPlayer, "acceleration", 2);
+        //     }
+        //     else if (Math.Abs(velocity.X) > Speed / 2 && Math.Abs(velocity.X) < Speed)
+        //         ShowSpecificFrame(animationPlayer, "acceleration", 3);
+        // }
+        // else if (Math.Abs(velocity.X) >= Speed)
+        // {
+        //     animationPlayer.Play("move");
+        // } 
+        
+        // if (velocity.X == 0)
+        // {
+        //     animationPlayer.Play("idle");
+        //     animationPlayer.FlipH = false;
+        // }
+        // if (inputDirection == Vector2.Zero && velocity.X != 0)
+        // {
+        //     if(velocity.X < 0)
+        //         velocity.X += Deceleration * (float)delta;
+        //     if (velocity.X > 0)
+        //         velocity.X -= Deceleration * (float)delta;
+        // }
+
+        // Velocity = velocity;
+		// MoveAndSlide();
 	}
     private static void ShowSpecificFrame(AnimatedSprite2D animationPlayer, string animation, int frame)
     {
