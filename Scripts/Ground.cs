577da484721623e92b3bbf6240584ccbe83fcdc9
Pnoenix <Ph03n1x.V7@gmail.com>
2026-03-06 13:06:00 +0100

    Worked on Ground API

--- Scripts/Ground.cs
+++ Scripts/Ground.cs
@@ -8,7 +8,8 @@ public partial class Ground : Node2D
 {
     private FastNoiseLite GroundNoise = new();
     private Vector2I lastTilePosition = Vector2I.MaxValue;
-    private HashSet<Vector2I> loadedTiles = new HashSet<Vector2I>();
+    private HashSet<Vector2I> loadedTiles = new();
+    private HashSet<Vector2I> UnbreakableTiles = new();
     [ExportGroup("Configurations")]
     [Export]
     public float CaveThreshold = 0.2f;
@@ -39,17 +40,32 @@ public partial class Ground : Node2D
         }
 
         GroundLayer.SetCellsTerrainConnect([
-                new Vector2I(0, 0),
-                new Vector2I(2, 0),
-                new Vector2I(3, 0),
-                new Vector2I(4, 0),
-                new Vector2I(5, 0),
-                new Vector2I(6, 0),
-                new Vector2I(7, 0),
-                new Vector2I(8, 0),
-                new Vector2I(9, 0),
-                new Vector2I(10, 0),
-            ], 0, 0);
+            new Vector2I(0, 0),
+            new Vector2I(2, 0),
+            new Vector2I(3, 0),
+            new Vector2I(4, 0),
+            new Vector2I(5, 0),
+            new Vector2I(6, 0),
+            new Vector2I(7, 0),
+            new Vector2I(8, 0),
+            new Vector2I(9, 0),
+            new Vector2I(10, 0),
+            new Vector2I(11, 0)
+        ], 0, 0);
+
+        BackgroundLayer.SetCellsTerrainConnect([
+            new Vector2I(0, 0),
+            new Vector2I(2, 0),
+            new Vector2I(3, 0),
+            new Vector2I(4, 0),
+            new Vector2I(5, 0),
+            new Vector2I(6, 0),
+            new Vector2I(7, 0),
+            new Vector2I(8, 0),
+            new Vector2I(9, 0),
+            new Vector2I(10, 0),
+            new Vector2I(11, 0)
+        ], 0, 0);
     }
 
 
@@ -92,6 +108,22 @@ public partial class Ground : Node2D
                     continue;
                 }
 
+                if (y <= 1)
+                {
+                    BackgroundLayer.SetCellsTerrainConnect(
+                        [localTilePosition], 
+                        0, 
+                        0
+                    );
+                    GroundLayer.SetCellsTerrainConnect(
+                        [localTilePosition], 
+                        0, 
+                        0
+                    );
+                    UnbreakableTiles.Add(localTilePosition);
+                    continue;
+                }
+
                 loadedTiles.Add(localTilePosition);
                 BackgroundLayer.SetCellsTerrainConnect(
                     [localTilePosition], 
@@ -111,5 +143,36 @@ public partial class Ground : Node2D
                 );
             }
         }
+    }
+
+
+    public Vector2I toTilePosition(Vector2 position)
+    {
+        float screenToTileFactor = 32.0f;
+        Vector2I tilePosition = new Vector2I(
+            (int)(position.X / screenToTileFactor),
+            (int)(position.Y / screenToTileFactor)
+        );
+
+        return tilePosition;
+    }
+
+
+    public bool IsUnbreakable(Vector2I tilePosition)
+    {
+        return UnbreakableTiles.Contains(tilePosition);
+    }
+
+
+    public bool BreakTile(Vector2I tilePosition)
+    {
+        if (UnbreakableTiles.Contains(tilePosition))
+            return false;
+
+        GroundLayer.SetCellsTerrainConnect(
+            [tilePosition], 0, 0
+        );
+
+        return true;
     }
 }
