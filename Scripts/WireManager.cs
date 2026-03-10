559adb2f01507abae70eaa6b648667ab7dd6347d
Lucas <lucasroemerisaksen@gmail.com>
2026-03-10 09:15:54 +0100

    wire

--- Scripts/WireManager.cs
+++ Scripts/WireManager.cs
@@ -7,7 +7,7 @@ public partial class WireManager : Node2D
     [Export] private Area2D wireTemplate;
 
     private float nextSpawnY;
-    private float spawnInterval = 32f;
+    private float spawnInterval = 4f;
 
     public override void _Ready()
     {
@@ -16,16 +16,20 @@ public partial class WireManager : Node2D
 
     public override void _Process(double delta)
     {
-        if (player.GlobalPosition.Y > nextSpawnY)
+
+    }
+
+    public void CheckIfNewNeeded()
+    {
+        var character = player.GetNode<CharacterBody2D>("CharacterBody2D");
+
+        if (character.GlobalPosition.Y + 20 > nextSpawnY)
         {
             CreateNew(new Vector2(GlobalPosition.X, nextSpawnY));
-            GD.Print("En er spawnet");
             nextSpawnY += spawnInterval;
-            GD.Print("Næste pos: " + nextSpawnY);
         }
-        else
-            GD.Print("CHecked but no, player X: "+ player.Transform.X + " Y: "+ player.Transform.Y);
     }
+
 
     public void CreateNew(Vector2 spawnPos)
     {
