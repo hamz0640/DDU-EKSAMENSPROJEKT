b0437d341864b94cd7ee54d28500b32fc367e1b9
Lucas <lucasroemerisaksen@gmail.com>
2026-03-10 10:41:14 +0100

    Wire V1

--- Scripts/WireManager.cs
+++ Scripts/WireManager.cs
@@ -6,7 +6,7 @@ public partial class WireManager : Node2D
     [Export] private Node2D player;
     [Export] private Area2D wireTemplate;
     [Export] private int startYpos = -21;
-
+    [Export] private Area2D wireArea;
     private float nextSpawnY;
     private float spawnInterval = 4f;
 
@@ -22,16 +22,31 @@ public partial class WireManager : Node2D
         //Hvis spilleren er længere nede end wire, lav wiren længere
         if (character.GlobalPosition.Y + 0 > nextSpawnY)
         {
-            CreateNew(new Vector2(GlobalPosition.X, nextSpawnY));
-            nextSpawnY += spawnInterval;
+            CreateNew();
         }
     }
 
-    public void CreateNew(Vector2 spawnPos)
+    public void CreateNew()
     {
+        nextSpawnY += spawnInterval;
         //Lav ny wire i forlængelse af den gamle HVIS IKKE den ville ramme en ground block
-        Area2D newArea = (Area2D)wireTemplate.Duplicate();
-        AddChild(newArea);
-        newArea.GlobalPosition = spawnPos;
+        bool touchingGround = false;
+        var bodies = wireArea.GetOverlappingBodies();
+        foreach (var body in bodies) {
+            if (body.Name == "GroundLayer")
+            {
+                touchingGround = true;
+            }
+        }
+        if (!touchingGround)
+        {
+            Area2D newArea = (Area2D)wireTemplate.Duplicate();
+            AddChild(newArea);
+            newArea.GlobalPosition = spawnPos;
+            //Flyt den detekterene del af wiren til spidsen
+            wireArea.GlobalPosition = spawnPos;
+        }
+        else
+            GD.Print("WIRE BLOCKED, the wire is touching the ground, so it is not to be extended");
     }
 }
