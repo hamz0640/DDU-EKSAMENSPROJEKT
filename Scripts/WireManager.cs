5ad4673338f858e77dd28c52f48394cd6231ad29
Lucas <lucasroemerisaksen@gmail.com>
2026-03-10 09:40:37 +0100

    no conflict

--- Scripts/WireManager.cs
+++ Scripts/WireManager.cs
@@ -5,39 +5,33 @@ public partial class WireManager : Node2D
 {
     [Export] private Node2D player;
     [Export] private Area2D wireTemplate;
+    [Export] private int startYpos = -21;
 
     private float nextSpawnY;
     private float spawnInterval = 4f;
 
     public override void _Ready()
     {
-        nextSpawnY = GlobalPosition.Y - spawnInterval;
-    }
-
-    public override void _Process(double delta)
-    {
-
+        //Original spawn Y koordinat
+        nextSpawnY = startYpos + spawnInterval;
     }
 
     public void CheckIfNewNeeded()
     {
         var character = player.GetNode<CharacterBody2D>("CharacterBody2D");
-
-        if (character.GlobalPosition.Y + 20 > nextSpawnY)
+        //Hvis spilleren er længere nede end wire, lav wiren længere
+        if (character.GlobalPosition.Y + 0 > nextSpawnY)
         {
             CreateNew(new Vector2(GlobalPosition.X, nextSpawnY));
             nextSpawnY += spawnInterval;
         }
     }
 
-
     public void CreateNew(Vector2 spawnPos)
     {
-
+        //Lav ny wire i forlængelse af den gamle HVIS IKKE den ville ramme en ground block
         Area2D newArea = (Area2D)wireTemplate.Duplicate();
         AddChild(newArea);
         newArea.GlobalPosition = spawnPos;
-
-        GD.Print($"New wire created at: {spawnPos}");
     }
 }
