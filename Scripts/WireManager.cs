705eca900f66db23085dd773e2fc69ce22f3a186
Lucas <lucasroemerisaksen@gmail.com>
2026-03-10 10:49:56 +0100

    wire

--- Scripts/WireManager.cs
+++ Scripts/WireManager.cs
@@ -5,48 +5,42 @@ public partial class WireManager : Node2D
 {
     [Export] private Node2D player;
     [Export] private Area2D wireTemplate;
-    [Export] private int startYpos = -21;
+    [Export] private int startYpos = -30;
     [Export] private Area2D wireArea;
-    private float nextSpawnY;
+    private Vector2 nextSpawn;
     private float spawnInterval = 4f;
 
     public override void _Ready()
     {
-        //Original spawn Y koordinat
-        nextSpawnY = startYpos + spawnInterval;
-    }
+        nextSpawn.Y = startYpos + spawnInterval;
+        nextSpawn.X = wireTemplate.GlobalPosition.X;
+    } // Original spawn position
 
     public void CheckIfNewNeeded()
     {
         var character = player.GetNode<CharacterBody2D>("CharacterBody2D");
-        //Hvis spilleren er længere nede end wire, lav wiren længere
-        if (character.GlobalPosition.Y + 0 > nextSpawnY)
+        if (character.GlobalPosition.Y + 10 > nextSpawn.Y)
         {
             CreateNew();
         }
-    }
+    } // Hvis spilleren er længere nede end wire, lav wiren længere
 
     public void CreateNew()
     {
-        nextSpawnY += spawnInterval;
-        //Lav ny wire i forlængelse af den gamle HVIS IKKE den ville ramme en ground block
         bool touchingGround = false;
         var bodies = wireArea.GetOverlappingBodies();
         foreach (var body in bodies) {
             if (body.Name == "GroundLayer")
-            {
                 touchingGround = true;
-            }
         }
         if (!touchingGround)
         {
+            nextSpawn.Y += spawnInterval;
             Area2D newArea = (Area2D)wireTemplate.Duplicate();
             AddChild(newArea);
-            newArea.GlobalPosition = spawnPos;
+            newArea.GlobalPosition = nextSpawn;
             //Flyt den detekterene del af wiren til spidsen
-            wireArea.GlobalPosition = spawnPos;
+            wireArea.GlobalPosition = nextSpawn;
         }
-        else
-            GD.Print("WIRE BLOCKED, the wire is touching the ground, so it is not to be extended");
-    }
+    } // Lav ny wire i forlængelse af den gamle HVIS IKKE den ville ramme en ground block
 }
