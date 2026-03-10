9e7fc7ef1f836650e401e68f508ddc70c3ea7018
Pnoenix <Ph03n1x.V7@gmail.com>
2026-03-10 11:38:31 +0100

    Add minerals

--- Mineral.cs
+++ Mineral.cs
@@ -19,33 +19,65 @@ public partial class Mineral : Node2D
     public void SetMineralType(MineralType mineralType)
     {
         Type = mineralType;
+        
         switch (Type) {
             case MineralType.Red:
-                Right.RegionRect = new Rect2(00, 00, new Vector2(32, 32));
+                Rect2 rect1 = new Rect2(00, 00, new Vector2(32, 32)); 
+                Right.RegionRect  = rect1;
+                Left.RegionRect   = rect1;
+                Top.RegionRect    = rect1;
+                Bottom.RegionRect = rect1;
                 break;
             case MineralType.Purple:
-                Right.RegionRect = new Rect2(00, 32, new Vector2(32, 32));
+                Rect2 rect2 = new Rect2(00, 32, new Vector2(32, 32)); 
+                Right.RegionRect  = rect2;
+                Left.RegionRect   = rect2;
+                Top.RegionRect    = rect2;
+                Bottom.RegionRect = rect2;
                 break;
             case MineralType.Yellow:
-                Right.RegionRect = new Rect2(00, 64, new Vector2(32, 32));
+                Rect2 rect3 = new Rect2(00, 64, new Vector2(32, 32)); 
+                Right.RegionRect  = rect3;
+                Left.RegionRect   = rect3;
+                Top.RegionRect    = rect3;
+                Bottom.RegionRect = rect3;
                 break;
         }
     }
 
-    public void ShowSide(string side)
+    public void ShowSide(Vector2I side)
     {
         switch (side) {
-            case "Right":
+            case Vector2I(1, 0):
                 Right.Show();
                 break;
-            case "Left":
+            case Vector2I(-1, 0):
                 Left.Show();
                 break;
-            case "Top":
+            case Vector2I(0, -1):
                 Top.Show();
                 break;
-            case "Bottom":
+            case Vector2I(0, 1):
                 Bottom.Show();
+                break;
+        }
+    }
+
+
+    public void HideSide(Vector2I side)
+    {
+        switch (side) {
+            case Vector2I(1, 0):
+                Right.Hide();
+                break;
+            case Vector2I(-1, 0):
+                Left.Hide();
+                break;
+            case Vector2I(0, -1):
+                Top.Hide();
+                break;
+            case Vector2I(0, 1):
+                Bottom.Hide();
                 break;
         }
     }
--- Scripts/Ground.cs
+++ Scripts/Ground.cs
@@ -1,20 +1,30 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public partial class Ground : Node2D
 {
     private FastNoiseLite GroundNoise = new();
     private Vector2I lastTilePosition = Vector2I.MaxValue;
     private HashSet<Vector2I> LoadedTiles = new();
+    private HashSet<Vector2I> PlacedTiles = new();
     private HashSet<Vector2I> UnbreakableTiles = new();
     private Dictionary<Vector2I, Mineral.MineralType> Minerals = new();
+    private Dictionary<Vector2I, Mineral> MineralNodeLUT = new();
+    private Random rand = new();
     public Dictionary<Vector2I, float> TileHealth = new();
     [ExportGroup("Configurations")]
     [Export]
     public float CaveThreshold = 0.2f;
     [Export]
     public int TileMargin = 1;
+    [Export]
+    public float RedProbability = 0.1f;
+    [Export]
+    public float PurpleProbability = 0.1f;
+    [Export]
+    public float YellowProbability = 0.1f;
     [ExportGroup("References")]
     [Export]
     public TileMapLayer BackgroundLayer = null;
@@ -24,8 +34,7 @@ public partial class Ground : Node2D
 
     public override void _Ready()
     {
-        Random random = new();
-        GroundNoise.Seed = random.Next();
+        GroundNoise.Seed = rand.Next();
         GroundNoise.NoiseType = FastNoiseLite.NoiseTypeEnum.Perlin;
         GroundNoise.Frequency = 0.05f;
 
@@ -49,22 +58,27 @@ public partial class Ground : Node2D
         PlaceGroundTile(new Vector2I(10, 0), true);
         PlaceGroundTile(new Vector2I(11, 0), true);
 
-        PlaceBackgroundTile(new Vector2I(0, 0), true);
-        PlaceBackgroundTile(new Vector2I(1, 0), true);
-        PlaceBackgroundTile(new Vector2I(2, 0), true);
-        PlaceBackgroundTile(new Vector2I(3, 0), true);
-        PlaceBackgroundTile(new Vector2I(4, 0), true);
-        PlaceBackgroundTile(new Vector2I(5, 0), true);
-        PlaceBackgroundTile(new Vector2I(6, 0), true);
-        PlaceBackgroundTile(new Vector2I(7, 0), true);
-        PlaceBackgroundTile(new Vector2I(8, 0), true);
-        PlaceBackgroundTile(new Vector2I(9, 0), true);
-        PlaceBackgroundTile(new Vector2I(10, 0), true);
-        PlaceBackgroundTile(new Vector2I(11, 0), true);
+        PlaceBackgroundTile(new Vector2I(0, 0));
+        PlaceBackgroundTile(new Vector2I(1, 0));
+        PlaceBackgroundTile(new Vector2I(2, 0));
+        PlaceBackgroundTile(new Vector2I(3, 0));
+        PlaceBackgroundTile(new Vector2I(4, 0));
+        PlaceBackgroundTile(new Vector2I(5, 0));
+        PlaceBackgroundTile(new Vector2I(6, 0));
+        PlaceBackgroundTile(new Vector2I(7, 0));
+        PlaceBackgroundTile(new Vector2I(8, 0));
+        PlaceBackgroundTile(new Vector2I(9, 0));
+        PlaceBackgroundTile(new Vector2I(10, 0));
+        PlaceBackgroundTile(new Vector2I(11, 0));
     }
 
 
     public override void _Process(double delta)
+    {
+        LoadNewTiles();
+    }
+
+    private void LoadNewTiles()
     {
         Camera2D camera = (Camera2D)GetTree().GetFirstNodeInGroup("Camera");
         Vector2 cameraPosition = camera.GlobalPosition;
@@ -98,23 +112,101 @@ public partial class Ground : Node2D
                 Vector2I localTilePosition = new Vector2I(x, y);
                 if (LoadedTiles.Contains(localTilePosition))
                     continue;
-                
 
-                if (y < 1)
+                PlaceBackgroundTile(localTilePosition);
+
+                if (y < 1 || (GroundNoise.GetNoise2D(x, y) < CaveThreshold))
                 {
-                    PlaceBackgroundTile(localTilePosition, true);
-                    PlaceGroundTile(localTilePosition, true);
-
-                    continue;
+                    PlaceGroundTile(localTilePosition, y < 1);
                 }
-
-                PlaceBackgroundTile(localTilePosition, false);
-
-                float value = GroundNoise.GetNoise2D(x, y);
-                if (value < CaveThreshold)
-                    PlaceGroundTile(localTilePosition, false);
             }
         }
+
+        for (int x = leftMostTile; x <= rightMostTile; x++)
+        {
+            for (int y = bottomMostTile; y >= 0; y--)
+            {
+                Vector2I localTilePosition = new Vector2I(x, y);
+                if (LoadedTiles.Contains(localTilePosition))
+                    continue;
+                
+                if (!PlacedTiles.Contains(localTilePosition))
+                    continue;
+
+                if (y >= 1)
+                    SpawnMinerals(localTilePosition);
+            }
+        }
+
+        for (int x = leftMostTile; x <= rightMostTile; x++)
+        {
+            for (int y = bottomMostTile; y >= 0; y--)
+            {
+                Vector2I localTilePosition = new Vector2I(x, y);
+                LoadedTiles.Add(localTilePosition);
+            }
+        }
+    }
+
+    private void SpawnMinerals(Vector2I tilePosition)
+    {
+        float mineralValue = rand.NextSingle();
+        Mineral.MineralType mineralType = Mineral.MineralType.Red;
+
+        float loRed = 0.0f;
+        float hiRed = loRed + RedProbability;
+        float loPurple = hiRed;
+        float hiPurple = loPurple + PurpleProbability;
+        float loYellow = hiPurple;
+        float hiYellow = loYellow + YellowProbability;
+
+
+        bool shouldInstantiate = false;
+        if (mineralValue > loRed && mineralValue < hiRed)
+        {
+            mineralType = Mineral.MineralType.Red;
+            shouldInstantiate = true;
+        }
+            
+        if (mineralValue > loPurple && mineralValue < hiPurple)
+        {
+            mineralType = Mineral.MineralType.Purple;
+            shouldInstantiate = true;
+        }
+        if (mineralValue > loYellow && mineralValue < hiYellow)
+        {
+            mineralType = Mineral.MineralType.Yellow;
+            shouldInstantiate = true;
+        }
+
+        if (!shouldInstantiate)
+            return;
+            
+        
+        PackedScene mineralScene = GD.Load<PackedScene>("res://Scenes/Mineral.tscn");
+        Mineral mineral = mineralScene.Instantiate<Mineral>();
+        AddChild(mineral);
+        
+        Minerals[tilePosition] = mineralType;
+        MineralNodeLUT[tilePosition] = mineral;
+        
+        mineral.SetMineralType(mineralType);
+        mineral.Position = tilePosition * 32 + new Vector2(16, 16);
+
+        if (!IsTileAt(tilePosition + new Vector2I(0, -1)))
+            mineral.ShowSide(Vector2I.Up);
+        if (!IsTileAt(tilePosition + new Vector2I(1, 0)))
+            mineral.ShowSide(Vector2I.Right);
+        if (!IsTileAt(tilePosition + new Vector2I(0, 1)))
+            mineral.ShowSide(Vector2I.Down);
+        if (!IsTileAt(tilePosition + new Vector2I(-1, 0)))
+            mineral.ShowSide(Vector2I.Left);
+    } 
+
+
+    public bool IsTileAt(Vector2I tilePosition)
+    {
+        return PlacedTiles.Contains(tilePosition);
     }
 
 
@@ -131,36 +223,57 @@ public partial class Ground : Node2D
 
 
     public bool BreakTile(Vector2I tilePosition)
-    {
+    {        
         if (UnbreakableTiles.Contains(tilePosition))
             return false;
+        
+        MineralNodeLUT.TryGetValue(tilePosition, out Mineral mineralCenter);
+        if (mineralCenter != null)
+        {
+            RemoveChild(mineralCenter);
+            Minerals.Remove(tilePosition);
+            MineralNodeLUT.Remove(tilePosition);
+        }
+
+        MineralNodeLUT.TryGetValue(tilePosition + Vector2I.Up,    out Mineral mineralUp);
+        MineralNodeLUT.TryGetValue(tilePosition + Vector2I.Down,  out Mineral mineralDown);
+        MineralNodeLUT.TryGetValue(tilePosition + Vector2I.Right, out Mineral mineralRight);
+        MineralNodeLUT.TryGetValue(tilePosition + Vector2I.Left,  out Mineral mineralLeft);
+
+        if (mineralUp    != null) mineralUp.ShowSide(Vector2I.Down);
+        if (mineralDown  != null) mineralDown.ShowSide(Vector2I.Up);
+        if (mineralRight != null) mineralRight.ShowSide(Vector2I.Left);
+        if (mineralLeft  != null) mineralLeft.ShowSide(Vector2I.Right);
 
         GroundLayer.SetCellsTerrainConnect([tilePosition], 0, -1);
 
         return true;
     }
 
-    public bool PlaceGroundTile(Vector2I tilePosition, bool isUnbreakable)
+    public void PlaceGroundTile(Vector2I tilePosition, bool isUnbreakable)
     {
-        LoadedTiles.Add(tilePosition);
+        MineralNodeLUT.TryGetValue(tilePosition + Vector2I.Up,    out Mineral mineralUp);
+        MineralNodeLUT.TryGetValue(tilePosition + Vector2I.Down,  out Mineral mineralDown);
+        MineralNodeLUT.TryGetValue(tilePosition + Vector2I.Right, out Mineral mineralRight);
+        MineralNodeLUT.TryGetValue(tilePosition + Vector2I.Left,  out Mineral mineralLeft);
+
+        if (mineralUp    != null) mineralUp.HideSide(Vector2I.Down);
+        if (mineralDown  != null) mineralDown.HideSide(Vector2I.Up);
+        if (mineralRight != null) mineralRight.HideSide(Vector2I.Left);
+        if (mineralLeft  != null) mineralLeft.HideSide(Vector2I.Right);
+
+        PlacedTiles.Add(tilePosition);
+
         if (isUnbreakable)
             UnbreakableTiles.Add(tilePosition);
         
         GroundLayer.SetCellsTerrainConnect([tilePosition], 0, 0);
         TileHealth[tilePosition] = 1.0f;
-        
-        return true;
     }
 
 
-    public bool PlaceBackgroundTile(Vector2I tilePosition, bool isUnbreakable)
-    {
-        LoadedTiles.Add(tilePosition);
-        if (isUnbreakable)
-            UnbreakableTiles.Add(tilePosition);
-        
+    public void PlaceBackgroundTile(Vector2I tilePosition)
+    {     
         BackgroundLayer.SetCellsTerrainConnect([tilePosition], 0, 0);
-        
-        return true;
     }
 }
