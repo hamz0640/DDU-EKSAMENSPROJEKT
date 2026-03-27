61d79f4769172ff1f36cfa54a23c6bcc43eb169d
Lucas <lucasroemerisaksen@gmail.com>
2026-03-27 12:23:00 +0100

    Turret scene

---
+++ Scripts/Turret.cs
@@ -0,0 +1,50 @@
+using Godot;
+using System;
+
+public partial class Turret : CharacterBody2D
+{
+	[Export] int SpawnHeight = 150;
+	[Export] AnimatedSprite2D animator;
+	[Export] int Shootspeed = 1;
+	bool deployed = false;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		animator.Pause();
+		GD.Print("Turret spawned");
+		this.Position = new Vector2(this.Position.X,SpawnHeight);
+		//Screenshake
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+        Vector2 velocity = Velocity;
+        if (!IsOnFloor())
+		{
+            velocity += GetGravity() * (float)delta;
+            Velocity = velocity;
+        }
+		else
+		{
+			if (!deployed)
+			{
+                animator.Play("deploy");
+				GetTree().CreateTimer(Shootspeed).Timeout += _TimeOutShoot;
+            }
+		}
+        MoveAndSlide();
+    }
+	private void _TimeOutShoot()
+	{
+		if (deployed)
+		{
+            animator.Play("shoot");
+            // Shoot logic spawn bullet and smth
+
+            // End shoot logic
+            GetTree().CreateTimer(Shootspeed).Timeout += _TimeOutShoot;
+        }
+	}
+}
