6b1f3fce028e32bcb36e72be02e2a481167ce9f9
Pnoenix <Ph03n1x.V7@gmail.com>
2026-04-11 13:32:11 +0200

    Added Interactable class

---
+++ Scripts/Interactable.cs
@@ -0,0 +1,52 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+public partial class Interactable : Area2D
+{
+    [Signal]
+    public delegate void InteractEventHandler();
+    [Export]
+    string InputActionName = "interact";
+    private bool IsPlayerInInteractableArea = false;
+
+
+    public override void _Ready()
+    {
+        BodyEntered += (body) =>
+        {
+            if (body is PlayerController) IsPlayerInInteractableArea = true;
+        };
+
+        BodyExited += (body) =>
+        {
+            if (body is PlayerController) IsPlayerInInteractableArea = false;
+        };
+    }
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (!(Input.IsActionJustPressed(InputActionName) && IsPlayerInInteractableArea))
+            return;
+        
+        Camera2D camera = (Camera2D)GetTree().GetFirstNodeInGroup("Camera");
+        Vector2 cameraPosition = camera.GlobalPosition;
+        
+        Array<Node> interactables = GetTree().GetNodesInGroup("Interactable");
+        Node2D closestInteractable = this;
+
+        for (int i = 0; i < interactables.Count; i++)
+        {
+            Node2D currentInteractable = (Node2D)interactables[i];
+            float currentInteractableDistance = cameraPosition.DistanceTo(currentInteractable.GlobalPosition);
+            float closestInteractableDistance = cameraPosition.DistanceTo(closestInteractable.GlobalPosition);
+
+            if (currentInteractableDistance < closestInteractableDistance)
+                closestInteractable = currentInteractable;
+        }
+
+        if (closestInteractable == this)
+            EmitSignal(SignalName.Interact);
+    }
+}
--- Scripts/UpgradeStation.cs
+++ Scripts/UpgradeStation.cs
@@ -6,15 +6,21 @@ using System.Collections.Generic;
 public partial class UpgradeStation : Node2D
 {
     [Export]
-    public Area2D InputChute = null;
+    public Interactable InputChute = null;
     [Export]
-    public Area2D UpgradeConsole = null;
+    public Interactable UpgradeConsole = null;
     [Export]
     public UpgradeMenu UpgradeMenu = null;
 
 
     public override void _Ready()
     {
+        InputChute.Interact += DepositMinerals;
+        UpgradeConsole.Interact += () =>
+        {
+            ToggleUpgradeConsole(!UpgradeMenu.Visible);
+        };
+
         Global global = Global.GetInstance();
         Array<Upgrade> upgrades = global.GetState<Array<Upgrade>>("Upgrades");
 
@@ -36,42 +42,20 @@ public partial class UpgradeStation : Node2D
     public override void _PhysicsProcess(double delta)
     {
         Camera2D camera = (Camera2D)GetTree().GetFirstNodeInGroup("Camera");
-        CharacterBody2D player = (CharacterBody2D)camera.GetParent();
-        
         Vector2 cameraPosition = camera.GlobalPosition;
-        Vector2 InputChutePosition = InputChute.GlobalPosition;
-        Vector2 UpgradeConsolePosition = UpgradeConsole.GlobalPosition;
 
-        float distanceToInputChute = cameraPosition.DistanceTo(InputChutePosition);
-        float distanceToUpgradeConsole = cameraPosition.DistanceTo(UpgradeConsolePosition);
-
-        bool closerToInputChute = distanceToInputChute < distanceToUpgradeConsole;
-
-        bool canDepositMinerals    = InputChute.GetOverlappingBodies().Contains(player) && closerToInputChute;
-        bool canOpenUpgradeConsole = UpgradeConsole.GetOverlappingBodies().Contains(player) && !closerToInputChute;
-
-        if (canDepositMinerals && Input.IsActionJustPressed("interact")) {
-            GD.Print("Deposited Minerals");
-            DepositMinerals();
-        }
-
-        if (canOpenUpgradeConsole && Input.IsActionJustPressed("interact"))
+        if (UpgradeConsole.GlobalPosition.DistanceTo(cameraPosition) > 23.0)
         {
-            ToggleUpgradeConsole();
-        }
-
-        if (distanceToUpgradeConsole > 21.0 && UpgradeMenu.Visible)
-        {
-            ToggleUpgradeConsole();
+            ToggleUpgradeConsole(false);
         }
     }
 
-    private void ToggleUpgradeConsole()
+    private void ToggleUpgradeConsole(bool visible)
     {
         CanvasLayer UI = (CanvasLayer)GetTree().GetFirstNodeInGroup("UI");
 
-        UI.Visible = !UI.Visible;
-        UpgradeMenu.Visible = !UpgradeMenu.Visible;
+        UI.Visible = !visible;
+        UpgradeMenu.Visible = visible;
     }
 
     private void DepositMinerals()
